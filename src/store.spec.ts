1339db1997444e8dd48ddfd77e6aadf8e4baf2f2
Damian Wood <damian@live.co.uk>
2017-02-07 01:36:07 +0000

    removed original mobx store

--- src/store.spec.ts
+++ src/store.spec.ts
@@ -1,59 +1,7 @@
 import {expect} from 'chai'
-import * as fetchMock from 'fetch-mock'
-import {autorun, toJS} from 'mobx'
+import {toJS} from 'mobx'
 
-import {PageStore, FavouritesStore} from './store'
-
-describe('PageStore', function() {
-  afterEach(function() {
-    fetchMock.restore()
-  })
-
-  it(`'loading' state immeditately after calling load()`, function() {
-    fetchMock.getOnce('*', {data: 'hello world'})
-    const ps = new PageStore()
-    ps.load('http://lol')
-    expect(ps.state).to.deep.equal({status: 'loading'})
-  })
-
-  it(`'error' state after request fails`, async function() {
-    fetchMock.getOnce('*', {status: 500})
-    const ps = new PageStore()
-    await ps.load('http://lol')
-    expect(ps.state).to.contain({status: 'error'})
-  })
-
-  it(`'done' state after request succeeds`, async function() {
-    const payload = {data: 'hello world'}
-    fetchMock.getOnce('*', payload)
-    const ps = new PageStore()
-    await ps.load('http://lol')
-    expect(ps.state).to.deep.equal({status: 'done', json: payload})
-  })
-
-  it(`state should be observable via mobx`, function(done) {
-    const ps = new PageStore()
-    let call = 0
-    const disposer = autorun(() => {
-      switch(call) {
-        case 0:
-          expect(ps.state.status).to.equal('loading')
-          // Can't change an observable from within autorun.
-          setTimeout(function() {
-            // Changing it should trigger autorun again.
-            ps.state.status = 'error'
-          }, 0)
-          break
-        case 1:
-          expect(ps.state.status).to.equal('error')
-          disposer()
-          done()
-          break
-      }
-      call++
-    })
-  })
-})
+import {FavouritesStore} from './store'
 
 describe('FavouritesStore', function() {
   it(`initial state is empty`, function() {
--- src/store.ts
+++ src/store.ts
@@ -1,27 +1,4 @@
-import {observable, autorun, ObservableMap} from 'mobx'
-
-// Approach inspired from an OCaml talk by a guy from Jane Street Capital.
-type State = {status: 'loading'}
-           | {status: 'done', json: any}
-           | {status: 'error', reason: string}
-
-export class PageStore {
-  @observable state: State = {status: 'loading'}
-
-  async load(url: string) {
-    this.state = {status: 'loading'}
-    try {
-      const resp = await fetch(url)
-      if (!resp.ok) {
-        throw resp.statusText
-      }
-      const json = await resp.json()
-      this.state = {status: 'done', json}
-    } catch (reason) {
-      this.state = {status: 'error', reason}
-    }
-  }
-}
+import {observable, ObservableMap} from 'mobx'
 
 export type ResourceFavourites = ObservableMap<FavouriteItem>
 type FavouriteStore = ObservableMap<ResourceFavourites>
